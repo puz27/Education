19c1ac945bc001e974a735a37278dc6282a6bf10
Avramenko <n.avramenko87@gmail.com>
2022-11-23 15:52:51 +0300

    add information

--- homework_3.py
+++ homework_3.py
@@ -11,45 +11,47 @@ count_false_answer = 0
 # counter amount questions
 count_questions = len(questions)
 
-print(count_questions)
 # Greeting
 print('Привет! Предлагаю проверить свои знания английского!', 'Наберите "ready", чтобы начать!', sep='\n')
+user_command = input()
+if user_command == 'ready':
 
-user_name = input()
-print(f"Привет, {user_name}, начинаем тренировку!")
+    for i in range(count_questions):
+        count_false_answer = 0
 
+        # check user answer
+        print(questions[i])
+        user_answer = input()
 
-for i in range(count_questions):
-    count_false_answer = 0
-    print(questions[i])
-    user_answer = input()
-
-    # check user answer
-    if user_answer != answers[i]:
         # if user false in answer. Give chance!
-        while count_false_answer < 3:
-            print(f"Осталось попыток: {3 - count_false_answer}, попробуйте еще раз!")
-            user_answer = input()
-            # check user answer
-            if user_answer != answers[i]:
-                count_false_answer += 1
+        if user_answer != answers[i]:
+            # user has 3 attemps
+            while count_false_answer < 3:
+                print(f"Осталось попыток: {3 - count_false_answer}, попробуйте еще раз!")
+                user_answer = input()
+                # check user answer
+                if user_answer != answers[i]:
+                    # numbers of user attemps
+                    count_false_answer += 1
+                else:
+                    # if user right in answer
+                    score_right_answer += (3 - count_false_answer)
+                    count_right_answer += 1
+                    print(f"Ответ верный! Вы получаете {3 - count_false_answer} баллов!", sep="\n")
+                    break
             else:
-                # if user right in answer
-                score_right_answer += (3 - count_false_answer)
-                count_right_answer += 1
-                print(f"Ответ верный! Вы получаете {3 - count_false_answer} баллов!", sep="\n")
-                break
+                print(f"Увы, но нет. Верный ответ: {answers[i]}", sep="\n")
+
+        # if user right in answer
         else:
-            print(f"Увы, но нет. Верный ответ: {answers[i]}", sep="\n")
+            print("Ответ верный!", "Вы получаете 10 баллов!", sep="\n")
+            score_right_answer += 10
+            count_right_answer += 1
 
-    # if user right in answer
-    else:
-        print("Ответ верный!", "Вы получаете 10 баллов!", sep="\n")
-        score_right_answer += 10
-        count_right_answer += 1
+    # Final information
+    print("")
+    print(f"Вот и все!", f"Вы ответили на {round(count_right_answer)} вопросов из {count_questions} верно.", sep="\n")
+    print(f"Вы заработали {score_right_answer} баллов.")
 
-# Final information
-percent = round(score_right_answer / (10 * count_questions) * 100)
-print("")
-print(f"Вот и все, {user_name}!", f"Вы ответили на {round(count_right_answer)} вопросов из {count_questions} верно.", sep="\n")
-print(f"Вы заработали {score_right_answer} баллов.", f"Это {percent} процентов.", sep="\n")
+else:
+    print('Кажется, вы не хотите играть. Очень жаль.')
