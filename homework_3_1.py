270d5645ad359c9e8711d288ea206926b206d2f8
Avramenko <n.avramenko87@gmail.com>
2022-11-23 15:31:19 +0300

    add information

--- homework_3_1.py
+++ homework_3_1.py
@@ -2,33 +2,33 @@
 questions = ["My name ___ Vova", "I ___ a coder", "I live ___ Moscow"]
 answers = ["is", "am", "in"]
 
-# counter user score
-score_right_answer = 0
 # counter user right answers
 count_right_answer = 0
 # counter amount questions
 count_questions = len(questions)
 
-print(count_questions)
 # Greeting
 print('Привет! Предлагаю проверить свои знания английского!', 'Наберите "ready", чтобы начать!', sep='\n')
+user_command = input()
+if user_command == 'ready':
 
-user_name = input()
-print(f"Привет, {user_name}, начинаем тренировку!")
+    # check user answer
+    for i in range(count_questions):
+        print(questions[i])
+        user_answer = input()
+        if user_answer != answers[i]:
+            print(f"Неправильно. Правильный ответ:{answers[i]}")
 
-# check user answer
-for i in range(count_questions):
-    print(questions[i])
-    user_answer = input()
-    if user_answer != answers[i]:
-        print(f"Неправильно. Правильный ответ:{answers[i]}")
+        else:
+            print("Ответ верный!")
+            count_right_answer += 1
 
-    else:
-        print("Ответ верный!", "Вы получаете 10 баллов!", sep="\n")
-        score_right_answer += 10
-        count_right_answer += 1
+    # Final information
+    percent = round(count_right_answer / count_questions * 100)
+    print()
+    print(f"Вот и все! Вы ответили на {count_right_answer} вопросов из {count_questions} верно.", sep="\n")
+    print(f"Это {percent} процентов.", sep="\n")
 
-# Final information
-percent = round(score_right_answer / (10 * count_questions) * 100)
-print(f"Вот и все, {user_name}!", f"Вы ответили на {round(count_right_answer)} вопросов из {count_questions} верно.", sep="\n")
-print(f"Вы заработали {score_right_answer} баллов.", f"Это {percent} процентов.", sep="\n")
+# user do not want to play
+else:
+    print('Кажется, вы не хотите играть. Очень жаль.')
