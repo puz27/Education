0e1ca4a40b4a02c8104d9df4f0d67ff3ccf782b9
Avramenko <n.avramenko87@gmail.com>
2022-11-27 23:58:10 +0300

    answers work use keys and value

--- homework_4.py
+++ homework_4.py
@@ -1,3 +1,4 @@
+# dictionaries of words
 words_easy = {
     "family": "семья",
     "hand": "рука",
@@ -19,6 +20,7 @@ words_hard = {
     "suggest": "предлагать",
     "except":"кроме",
 }
+# dictionaries to check user level
 levels = {
    0: "Нулевой",
    1: "Так себе",
@@ -27,11 +29,16 @@ levels = {
    4: "Хорошо",
    5: "Отлично",
 }
-
-user_answer = {}
+# with this dictionary user will work after he select a level
 user_words = {}
-user_difficult = ('1', 'средний', 'сложный')
+# dictionary all user answers
+final_user_answer = {}
+# options of user game level
+user_difficult = ('легкий', 'средний', 'сложный')
+# number right user answers
+user_right_answer = 0
 
+# Select level
 while True:
     print('Выберите уровень сложности.', 'Легкий, средний, сложный.', sep='\n')
     user_input = input()
@@ -48,14 +55,32 @@ while True:
         user_words = words_hard
         break
 
+# check user answers and add results in final_user_answer
 for key, value in user_words.items():
-
     print(f'{key}, {len(value)} букв, начинается на {value[0]}')
     user_answer = input().lower()
     if user_answer == user_words[key]:
+        final_user_answer[key] = final_user_answer.get(key, 'True')
         print(f'Верно, {key} — это {value}.')
         print()
 
     else:
+        final_user_answer[key] = final_user_answer.get(key, 'False')
         print(f'Неверно. {key} — это {value}.')
         print()
+
+# print result information
+print("Правильно отвечены слова:")
+for answer in final_user_answer:
+   if final_user_answer.get(answer) == 'True':
+       user_right_answer += 1
+       print(answer)
+print("Неправильно отвечены слова:")
+
+for answer in final_user_answer:
+    if final_user_answer.get(answer) == 'False':
+        print(answer)
+
+print()
+if user_right_answer in levels:
+    print("Ваш ранг:", levels[user_right_answer], sep='\n')
