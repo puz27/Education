38e01dcdfde8fa4fb30b3a9acaf0d559461d0a6e
anv <anv@woori.ru>
2022-12-22 13:41:22 +0300

    Add all function and comments. Tested.

--- homework_6/main_script.py
+++ homework_6/main_script.py
@@ -1,32 +1,55 @@
 import os
 import random
 
+# counter right user answers
+count_right_answer = 0
+
+
 def change_word(word):
+    """Mix letter in word"""
     slovo_as_list = random.sample(word, len(word))
     return(''.join(slovo_as_list))
 
+
 def read_words_from_file(file_name):
+    """Read words from file, add it to list for user questions"""
     user_questions = []
-    path_file_questions = os.path.join(file_name)
+    path_file_questions = os.path.join(os.getcwd(), file_name)
     with open(path_file_questions) as file:
         for i in file:
             user_questions.append(i.rstrip())
     return (user_questions)
 
+
 def write_statistic_to_file(user_name, count_right_answer, file_name):
+    """Write user name and user counts to file"""
     user_statistic = user_name + ' ' + str(count_right_answer) + '\n'
-    path_file_statistic = os.path.join(file_name)
+    path_file_statistic = os.path.join(os.getcwd(), file_name)
     with open(path_file_statistic, 'a') as file:
         file.write(user_statistic)
 
-#print("Введите ваше имя")
-user_name = input()
 
-# counter for answer
-count_right_answer = 0
+def show_statistic_from_file(file_name):
+    """Take information from file with game statistic and show it"""
+    counter_games = 0
+    # list where we add all results and after will find best result
+    user_results = []
+    path_file_statistic = os.path.join(os.getcwd(), file_name)
+    with open(path_file_statistic, 'r') as file:
+        for i in file:
+            counter_games += 1
+            user_result = i.rstrip().split(' ')
+            user_results.append(user_result[1])
+        return str(counter_games), max(user_results)
 
+
+# start game
+user_name = input("Введите ваше имя\n")
+
+# read words from file
 user_question = read_words_from_file("words.txt")
 
+# user answer on questions. count the number of user points
 for i in range(len(user_question)):
     print(user_question[i])
     print(f"Угадайте слово: {change_word(user_question[i])}")
@@ -36,14 +59,9 @@ for i in range(len(user_question)):
     else:
         print(f"Неверно! Верный ответ – {user_question[i]}")
 
+# write user game statistic to file
 write_statistic_to_file(user_name, count_right_answer, "history.txt")
 
-counter_games = 0
-user_results = []
-with open("history.txt", 'r') as file:
-    for i in file:
-        counter_games += 1
-        user_result = i.rstrip().split(' ')
-        user_results.append(user_result[1])
-    print(counter_games)
-print(max(user_results))
+# read & show user game statistic
+statistic_from_file = show_statistic_from_file("history.txt")
+print(f"\nВсего игр сыграно: {statistic_from_file[0]}\nВаш результат: {count_right_answer}\nМаксимальный рекорд: {statistic_from_file[1]}")
