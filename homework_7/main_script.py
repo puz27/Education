b2bfcc95d08376ca4b9a1d6395d300fd80e0fee0
Nikolay <n.avramenko87@gmail.com>
2022-12-28 16:58:45 +0300

    Work without checking.

--- homework_7/main_script.py
+++ homework_7/main_script.py
@@ -8,10 +8,28 @@ professions = json_load_data("professions.json")
 #print((load_professions('Frontend', professions)))
 
 # take data about student and work with it
-one_student = get_student_by_pk(1, students)
-one_profession = load_professions('Backend', professions)
+#one_student = get_student_by_pk(1, students)
+#student_skills = load_professions('Backend', professions)
 
 #print(one_student['skills'])
 #print(one_profession['skills'])
 
-print(check_fitness(one_student, one_profession))
+#print(check_fitness(one_student, one_profession))
+
+student_number = int(input("Введите номер студента\n"))
+
+# dictionary of all student information
+student_information = (get_student_by_pk(student_number, students))
+
+print(student_information)
+print(f"Студент: {student_information['full_name']}", f"Знает: {', '.join(student_information['skills'])}", sep='\n')
+student_needed_profession = input(f"Выберите специальность для оценки студента {student_information['full_name']}\n")
+
+# load from dictionary of skills that we want to find
+dictionary_needed_skills = load_professions(student_needed_profession, professions)
+
+#print(check_fitness(student_information, dictionary_needed_skills))
+student_know, student_not_know, fitness = (check_fitness(student_information, dictionary_needed_skills))
+print(f"Пригодность: {fitness}")
+print(f"{student_information['full_name']} знает: {', '.join(student_know)}")
+print(f"{student_information['full_name']} не знает: {', '.join(student_not_know)}")
--- homework_7/utils.py
+++ homework_7/utils.py
@@ -24,15 +24,19 @@ def load_professions(title, dictionary):
         if profession['title'] == title:
             return profession
 
-def check_fitness(student, profession):
+def check_fitness(student_information, dictionary_needed_skills):
     '''Take information about student skills and compare it with needed skills for profession'''
     user_compare_information = {}
 
-    skills_student = set(student['skills'])
-    skills_need = set(profession['skills'])
+    diff_student = student_information
+    diff_skills = dictionary_needed_skills
+
+    skills_student = set(diff_student['skills'])
+    skills_need = set(diff_skills['skills'])
 
     # student do not have this skills
     skills_not_known = skills_need.difference(skills_student)
+    skills_known = skills_need.intersection(skills_student)
     # print("у пользователя:", skills_student, len(skills_student))
     # print("пользователь не знает:", skills_not_known, len(skills_not_known))
     # print("все что нужно знать:", skills_need, len(skills_need))
@@ -41,8 +45,8 @@ def check_fitness(student, profession):
     fit_percent = round(100 - ((100 / len(skills_need)) * len(skills_not_known)))
 
     # add information in dictionary
-    user_compare_information['has'] = list(skills_student)
+    user_compare_information['has'] = list(skills_known)
     user_compare_information['lacks'] = list(skills_not_known)
     user_compare_information['fit_percent'] = fit_percent
 
-    return user_compare_information
+    return tuple(user_compare_information.values())
