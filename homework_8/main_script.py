63211e98ebb9c1756f777d3058eb9eee544b2788
Nikolay <n.avramenko87@gmail.com>
2023-01-12 00:43:08 +0300

    End variant.2023-01-12 With main module.

--- homework_8/main_script.py
+++ homework_8/main_script.py
@@ -1,33 +1,3 @@
-import random
-from utils import *
-from classes import *
+from utils import main
 
-# загрузка информации из json
-questions_information = json_load_data("questions.json")
-
-# создаем список с экземплярами вопросов и нужной информации для обработки этих вопросов
-questions_information_list = []
-for question in questions_information:
-    questions_information_list.append(Question(question['q'], question['d'], question['a']))
-
-# проверка заданы ли все вопросы
-while all(check_question_status(questions_information_list)) is False:
-
-    # рандомно тянем экземпляр из списка для обработки
-    current_question = random.choice(questions_information_list)
-
-    # задаем вопрос, если он еще не был задан
-    if current_question.is_question_asked is False:
-        print("Вопрос: {}\nСложность: {}/5".format(current_question.user_question, current_question.question_difficult))
-        current_question.user_answer = input()
-        print(current_question.build_feedback())
-
-# вывод статистики
-print(load_user_statistic(questions_information_list))
-
-
-
-
-
-
-
+main()
--- homework_8/utils.py
+++ homework_8/utils.py
@@ -1,5 +1,7 @@
 import os
 import json
+import random
+from classes import *
 
 
 def json_load_data(file_name):
@@ -34,3 +36,39 @@ def check_question_status(questions_information_list):
     for i in questions_information_list:
         data.append(i.is_question_asked)
     return data
+
+
+def main():
+    '''
+    Основная обработка взаимодействия с пользователем
+    '''
+    # загрузка информации из json
+    questions_information = json_load_data("questions.json")
+
+    # создаем список с экземплярами вопросов и нужной информации для обработки этих вопросов
+    questions_information_list = []
+    for question in questions_information:
+        questions_information_list.append(Question(question['q'], question['d'], question['a']))
+
+    # проверка заданы ли все вопросы
+    while all(check_question_status(questions_information_list)) is False:
+
+        # рандомно тянем экземпляр из списка для обработки
+        current_question = random.choice(questions_information_list)
+
+        # задаем вопрос, если он еще не был задан
+        if current_question.is_question_asked is False:
+            print("Вопрос: {}\nСложность: {}/5".format(current_question.user_question,
+                                                       current_question.question_difficult))
+            current_question.user_answer = input()
+            print(current_question.build_feedback())
+
+    # вывод статистики
+    print(load_user_statistic(questions_information_list))
+
+
+
+
+
+
+
