761db38e1bc7e1da6eabf772d642e9123894652e
anv <anv@woori.ru>
2023-01-16 18:47:31 +0300

    Add encoding for open file 2

--- Kursovaya_2/classes.py
+++ Kursovaya_2/classes.py
@@ -6,26 +6,31 @@ class BasicWord:
         self.word = word
         self.subwords = subwords
 
-    def check_subwords(self) -> bool:
-        pass
+    def check_subwords(self, word) -> bool:
+        return True if word in self.subwords else False
 
     def check_count_subwords(self) -> int:
+       return len(self.subwords)
+
+    def __repr__(self):
         pass
 
-    # def __repr__(self):
-    #     return self.word
 
 class Player:
 
-    def __init__(self, name, used_words = ''):
+    def __init__(self, name: str):
         self.name = name
-        self.used_words = used_words
+        self.used_words = []
 
-    def get_used_words(self) -> int:
-        pass
 
-    def add_word_to_used(self) -> NoReturn:
-        pass
+    def get_count_used_words(self) -> int:
+        return len(self.used_words)
 
-    def check_already_used(self) -> bool:
-        pass
+    def add_word_to_used(self, add_word) -> NoReturn:
+        self.used_words.append(add_word)
+
+    def check_already_used(self, word) -> bool:
+        return True if word in self.used_words else False
+
+    def __repr__(self):
+        return 'Отгаданные слова: {}'.format(' '.join(self.used_words))
--- Kursovaya_2/utils.py
+++ Kursovaya_2/utils.py
@@ -2,20 +2,37 @@ import requests
 import random
 from classes import *
 import json
+import os
 
-
-def get_data(address: str) -> list:
-
-    requests.urllib3.disable_warnings()
-    response = requests.get(address, verify = False)
-    data = response.json()
-    return data
+def json_load_data(file_name):
+    '''
+    Считываем данные из файла
+    '''
+    path_file = os.path.join(os.getcwd(), file_name)
+    file = open(path_file, 'r', encoding='utf-8')
+    data = random.choice(json.load(file))
+    file.close()
+    user_word = BasicWord((data['word']), data['subwords'])
+    return user_word
 
 
 def load_random_word(address: str) -> BasicWord:
-    data = random.choice(get_data(address))
-    user_word = BasicWord((data['word']), data['subwords'])
-    return user_word
+    '''
+    load data and create instance of BasicWord class
+    '''
+    requests.urllib3.disable_warnings()
+    response = requests.get(address, verify=False)
+
+    if response.status_code is 200:
+        data = random.choice(response.json())
+        user_word = BasicWord((data['word']), data['subwords'])
+        return user_word
+    else:
+        print('Нет данных')
+        exit()
+
+def get_statistic(user_data):
+    return '{}\nКоличество отгаданных слов: {}'.format(user_data.__str__(), user_data.get_count_used_words())
 
 
 
